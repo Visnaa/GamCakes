ea96195ceca2d7490382f9c9ffde42981e8f2949
Visnaa <visnaayt@gmail.com>
2021-03-12 15:20:56 +0100

    Added dolomite cake

--- src/main/java/com/visnaa/gemcakes/init/ModBlocks.java
+++ src/main/java/com/visnaa/gemcakes/init/ModBlocks.java
@@ -32,4 +32,5 @@ public class ModBlocks
     public static final RegistryObject<CakeBlock> MOON_STONE_CAKE = BLOCKS.register("moon_stone_cake", () -> new CakeBlock(Block.Properties.create(Material.CAKE).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.CLOTH)));
     public static final RegistryObject<CakeBlock> SUN_STONE_CAKE = BLOCKS.register("sun_stone_cake", () -> new CakeBlock(Block.Properties.create(Material.CAKE).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.CLOTH)));
     public static final RegistryObject<CakeBlock> CITRINE_CAKE = BLOCKS.register("citrine_cake", () -> new CakeBlock(Block.Properties.create(Material.CAKE).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.CLOTH)));
+    public static final RegistryObject<CakeBlock> DOLOMITE_CAKE = BLOCKS.register("dolomite_cake", () -> new CakeBlock(Block.Properties.create(Material.CAKE).hardnessAndResistance(0.5f, 0.5f).sound(SoundType.CLOTH)));
 }
